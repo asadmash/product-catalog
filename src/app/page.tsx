707aece1bbaa3a113a05d7e28ae13f1cb7833579
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-27 20:38:47 +0600

    show product list in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import ProductCard from "@/components/ProductCard";
+import { fetchAllProducts, Product } from "@/lib/api/products";
 
-export default function page() {
+export default async function page() {
+   let allProducts: Product[] = [];
+  try {
+    allProducts = await fetchAllProducts();
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
   return (
-    <div>
-      main page
-    </div>
+
+    <main className="p-6">
+      <div className="flex justify-between items-center flex-col sm:flex-row mb-6">
+        <h1 className="text-3xl font-bold">All Products</h1>
+
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {allProducts.map((product: any) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </main>
+
   )
 }
