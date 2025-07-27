96bda7227ed15cf444a17c330c79330900a89a91
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-27 22:45:40 +0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,26 @@ import SearchBar from "@/components/SearchBar";
 import FilteredProductList from "@/components/FilteredProductList";
 import { Suspense } from "react";
 
-
-export default async function page() {
-   let allProducts: Product[] = [];
+export default async function Page() {
+  let allProducts: Product[] = [];
   try {
     allProducts = await fetchAllProducts();
   } catch (error) {
     console.error("Error fetching products:", error);
   }
 
-
   return (
-
     <main className="p-6">
       <div className="flex justify-between items-center flex-col sm:flex-row mb-6">
         <h1 className="text-3xl font-bold">All Products</h1>
-<SearchBar />
+        <Suspense fallback={<div>Loading search...</div>}>
+          <SearchBar />
+        </Suspense>
       </div>
 
-      
-        <Suspense fallback={<div>Loading search...</div>}>
+      <Suspense fallback={<div>Loading search...</div>}>
         <FilteredProductList allProducts={allProducts} />
       </Suspense>
     </main>
-
-  )
+  );
 }
