d3ef096f707b4f0c421a9558f050f656c73c724f
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-27 20:33:29 +0600

    code product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { Product } from "@/lib/api/products";
+import Image from "next/image";
 
-export default function ProductCard() {
+
+export default function ProductCard({ product }: { product: Product }) {
   return (
-    <div>
-      
+ <div className="p-4 shadow-xl bg-[#fff] rounded-lg flex flex-col justify-around">
+      <div className="">
+        <Image
+          src={product.image}
+          alt={product.title}
+          width={300}
+          height={300}
+          objectFit="contain"
+          className="w-full h-auto max-h-[400px] object-contain"
+        />
+      </div>
+
+      <div className="p-4 self-end justify-end">
+        <h3 className="text-lg font-semibold line-clamp-2">{product.title}</h3>
+        <p className="text-sm text-gray-600 line-clamp-2">
+          {product.description}
+        </p>
+        <div className="mt-2 text-blue-600 font-bold">$ {product.price}</div>
+        <Link
+          href={`/product/${product.id}`}
+          className="mt-4 inline-block text-sm text-white bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded"
+        >
+          View Details
+        </Link>
+      </div>
     </div>
   )
 }
