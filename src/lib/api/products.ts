ee4f6014899cac33d7cbc208b614b35f786ba3d9
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-27 20:27:49 +0600

    fetch products from fakestoreapi

--- src/lib/api/products.ts
+++ src/lib/api/products.ts
@@ -1,4 +1,4 @@
-// Define the shape of product 
+// Define the shape of product
 export type Product = {
   id: number;
   title: string;
@@ -6,3 +6,22 @@ export type Product = {
   description: string;
   image: string;
 };
+
+export async function fetchProductById(id: string | number) {
+  const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+  if (!res.ok) {
+    throw new Error("Product not found");
+  }
+
+  return res.json();
+}
+
+export async function fetchAllProducts() {
+  const res = await fetch("https://fakestoreapi.com/products");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
+  return res.json();
+}
