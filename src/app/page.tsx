9c48efbe14157e821dc57212ed0aa2f627571f1a
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-27 21:55:29 +0600

    imp: place searchbar inside homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default async function page({ searchParams }: Props) {
     <main className="p-6">
       <div className="flex justify-between items-center flex-col sm:flex-row mb-6">
         <h1 className="text-3xl font-bold">All Products</h1>
-
+<SearchBar />
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
