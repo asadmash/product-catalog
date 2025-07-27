f6bfa7e3854ffa31f84aba0bc531759b712349db
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-27 20:56:51 +0600

    Imp: modify product page to use cart context

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -3,11 +3,13 @@
 import { notFound, useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import { useCart } from "@/context/CartContext";
 
 import { Product, fetchProductById } from "@/lib/api/products";
 
 export default function ProductPage() {
   const { id } = useParams();
+    const { dispatch } = useCart();
 
   const [product, setProduct] = useState<Product | null>(null);
   const [loading, setLoading] = useState(true);
@@ -33,6 +35,20 @@ export default function ProductPage() {
   if (loading) return <p>Loading product...</p>;
   if (!product) return <p>Product not found.</p>;
 
+// handle add to cart function
+ const handleAddToCart = () => {
+    dispatch({
+      type: "ADD_ITEM",
+      payload: {
+        id: product.id,
+        name: product.title,
+        price: product.price,
+        image: product.image,
+      },
+    });
+  };
+
+
   return (
     <div className="max-w-5xl mx-auto px-4 py-8 md:pt-20 grid md:grid-cols-2 gap-10">
       <div className="relative w-full h-[300px] md:h-[400px] bg-white rounded-2xl">
@@ -49,6 +65,15 @@ export default function ProductPage() {
         <p className="text-gray-600">{product.description}</p>
         <p className="text-xl font-semibold text-green-600">${product.price}</p>
       </div>
+
+        <div className="flex gap-4">
+          <button
+            className="px-4 py-2 bg-blue-600 text-white rounded-xl hover:bg-blue-700 transition"
+            onClick={handleAddToCart}
+          >
+            Add to Cart
+          </button>
+        </div>
     </div>
   );
 }
