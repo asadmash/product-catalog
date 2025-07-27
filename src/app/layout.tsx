1f869b33323a8ccc1508ad3f463cb6f599e3edd6
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-27 20:12:21 +0600

    make navbar client component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import React from "react";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({children} : {children: React.ReactNode}){
   return (
     <html lang="en">
       <body className="min-h-screen flex-col">
+        <Navbar/>
         <main className="flex-grow container mx-auto px-4 py-6">
 {children}
         </main>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,30 @@
-import Link from 'next/link'
-import React, { useState } from 'react'
+'use client';
+import Link from "next/link";
+import React, { useState } from "react";
 
 export default function Navbar() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <nav className='bg-gray-800 text-white p-4 flex justify-between items-center'>
-      <Link href="/" className="text-xl font-bold">Product Catalog</Link>
-       <div>
-        <Link href='/' className=" mr-4 hover:underline">
-        Home
+    <nav className="bg-gray-800 text-white p-4 flex justify-between items-center">
+      <Link href="/" className="text-xl font-bold">
+        Product Catalog
+      </Link>
+      <div>
+        <Link href="/" className=" mr-4 hover:underline">
+          Home
         </Link>
 
         {isLoggedIn ? (
-            <button onClick={() => setIsLoggedIn(false)}>
-                Logout
-            </button>
+          <button onClick={() => setIsLoggedIn(false)}>Logout</button>
         ) : (
-            <Link href={`/login`} className="bg-blue-600 px-3 py-1 rounded hover:bg-blue-700">
+          <Link
+            href={`/login`}
+            className="bg-blue-600 px-3 py-1 rounded hover:bg-blue-700"
+          >
             Login
-            </Link>
+          </Link>
         )}
       </div>
     </nav>
-  )
+  );
 }
